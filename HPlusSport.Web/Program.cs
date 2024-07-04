dd6998673b0efa45f59a1f23373ac6ae5ab967cb
Hermilo Ortega <hermilo.jh@gmail.com>
2024-07-03 21:11:59 -0700

    03_08 - Adding token authentication

--- HPlusSport.Web/Program.cs
+++ HPlusSport.Web/Program.cs
@@ -1,7 +1,8 @@
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using HPlusSport.Web.Data;
 using HPlusSport.Web;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("HPlusSportWebContextConnection") ?? throw new InvalidOperationException("Connection string 'HPlusSportWebContextConnection' not found.");
 
@@ -11,6 +12,24 @@ builder.Services.AddDefaultIdentity<HPlusSportWebUser>(options => options.SignIn
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        var key = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes("V€r¥ $ecret (not!)|V€r¥ $ecret (not!)|V€r¥ $ecret (not!)"));
+        options.SaveToken = true;
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            IssuerSigningKey = key
+        };
+    });
+
 
 var app = builder.Build();
 
@@ -27,8 +46,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
